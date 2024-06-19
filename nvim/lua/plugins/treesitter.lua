40967536f25603d8e2741a49cfb79ef87089c22c
Zyhong Liu <zyhongcha@gmail.com>
2024-06-19 19:48:53 +0200

    SQUASHME

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,28 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		tag = "v0.9.1",
+		opts = {
+			ensure_installed = {
+				"javascript",
+				"typescript",
+				"tsx",
+				"css",
+				"gitignore",
+				"graphql",
+				"php",
+				"http",
+				"json",
+				"scss",
+				"sql",
+				"vim",
+				"lua",
+			},
+			query_linter = {
+				enable = true,
+				use_virtual_text = true,
+				lint_events = { "BufWrite", "CursorHold" },
+			},
+		},
+	},
+}
